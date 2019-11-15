4350c0055f0ddfad3132098def8db4c527952881
Cristian Abramchuk <abramchukcristian@gmail.com>
2019-11-15 18:07:21 -0200

    headers request

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 const bodyParser =  require('body-parser');
 
 const app = express();
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 mongoose.connect("mongodb+srv://user:user@omnistack9-u7pfh.mongodb.net/test?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
